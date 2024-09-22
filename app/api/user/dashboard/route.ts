6cf48ad9a7e44930c72a9263214e55e09c761d9c
Restyled.io <commits@restyled.io>
2024-09-22 19:29:50 +0000

    Restyled by whitespace

--- app/api/user/dashboard/route.ts
+++ app/api/user/dashboard/route.ts
@@ -36,7 +36,7 @@ export async function GET() {
       db.learning.count(),
       db.quiz.count(),
       db.quizAttempt.findMany({
-        where: { 
+        where: {
           userId: user.id,
           completed: true
         },
